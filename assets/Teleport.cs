f2c2257c01e7d466b38a9f397fa3fbc19a5fa048
Ramon van Tricht <r.van.tricht@home.nl>
2017-02-27 14:16:05 +0100

    assets van ramon

---
+++ assets/Teleport.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+
+    IEnumerator OnTriggerEnter(Collider other)
+    {
+
+        if (someGlobals.trig == false)
+        {
+            if (other.tag == "Player")
+            {
+                someGlobals.trig = true;
+                GetComponent<AudioSource>().Play();
+                yield return new WaitForSeconds(2 / 3);
+                other.transform.position = new Vector3(24, 5, 32);
+            }
+        }
+    }
+
+    IEnumerator OnTriggerExit(Collider other)
+    {
+        yield return new WaitForSeconds(2);
+        someGlobals.trig = false;
+    }
+}
+
+/*
+IEnumerator OnTriggerEnter(Collider other) {
+        yield return new WaitForSeconds(2/3);
+        if (other.gameObject.tag == "Player")
+        {
+            GetComponent<AudioSource>().Play();
+            other.transform.position = new Vector3(24, 5, 32);
+            }
+        yield return new WaitForSeconds(2);
+    }
+}
+*/

6ca59dd26c7f03cf5857dc081cfd8ffd8feb9691
360snipersnoscop3getreckt <ijsselhof@outlook.com>
2017-03-03 22:31:40 +0100

    nieuwe scene natuurkunde lokaal

--- escape game bogerman/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
+++ escape game bogerman/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
@@ -61,7 +61,6 @@ namespace UnityStandardAssets.Characters.FirstPerson
         // Update is called once per frame
         private void Update()
         {
-			
             RotateView();
             // the jump state needs to read here to make sure it is not missed
             if (!m_Jump)
@@ -83,7 +82,6 @@ namespace UnityStandardAssets.Characters.FirstPerson
 
             m_PreviouslyGrounded = m_CharacterController.isGrounded;
         }
-
 
 
         private void PlayLandingSound()
